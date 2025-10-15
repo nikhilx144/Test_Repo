6889dc3fb4caa0c226e11784ea554fcc9626bc83
Nikhil <nikhilgelli1@gmail.com>
2025-10-16 03:02:24 +0530

    seventh commit (successful docker integration)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "We have successfully built a project with docker and jenkins!\nA docker image is made every time new changes to the code are pushed upstream, then the docker image is pushed to dockerhub and finally a container is run from the image."
+    return "We have successfully built a project with docker and jenkins!\nA docker image is made every time new changes to the code are pushed upstream, a docker image of the repo is pushed to dockerhub and finally a container is run from the image."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5100)
